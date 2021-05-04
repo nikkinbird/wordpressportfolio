64c9a3dc1187362ce7650b05bbdd5d84ded8ad9e
nikkinbird <nikki.bollman@gmail.com>
2021-05-03 20:40:55 -0500

    Updated social media buttons on front page

--- wp-content/themes/skillcrushstarter/front-page.php
+++ wp-content/themes/skillcrushstarter/front-page.php
@@ -15,13 +15,9 @@ get_header(); ?>
 			<?php while ( have_posts() ): the_post(); ?>
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
-				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
-					<a href="" class="soc-icon tw"><span class="screen-reader-text">Twitter</span></a>
-					<a href="" class="soc-icon fb"><span class="screen-reader-text">Facebook</span></a>
-					<a href="" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
-					<a href="" class="soc-icon db"><span class="screen-reader-text">Dribble</span></a>
-					<a href="" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
-					<a href="" class="soc-icon in"><span class="screen-reader-text">Instagram</span></a>
+				<div class="social-btns">  
+					<a href="https://www.linkedin.com/in/nikkibollman/" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
+					<a href="https://github.com/nikkinbird" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
 				</div>
 				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
 			<?php endwhile; ?>
