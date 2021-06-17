a766d96b1e40d7a64dc02dd761a0504a9333b570
nikkinbird <nikki.bollman@gmail.com>
2021-06-17 07:36:41 -0500

    Added front-page.php to child theme

---
+++ wp-content/themes/twentytwenty-theme-child/front-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * @package WordPress
+ * @subpackage Skillcrush_Starter
+ * @since Skillcrush Starter 2.0
+ */
+
+get_header(); ?>
+
+<section class="home-page">
+	<div class="main-content">
+		<div class="content">
+			<?php while ( have_posts() ): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+				<div class="social-btns">  
+					<a href="https://www.linkedin.com/in/nikkibollman/" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
+					<a href="https://github.com/nikkinbird" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
+				</div>
+				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
